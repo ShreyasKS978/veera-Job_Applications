febe87d5524ecb33aba7c864f1dddaafc86fee46
Shreyas <shreyasks978@gmail.com>
2025-07-08 13:56:32 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const sharp = require('sharp');
 const PDFDocument = require('pdfkit');
 
 const app = express();
-const port = 3038;
+const port = 3036;
 
 // PostgreSQL configuration
 const pool = new Pool({
